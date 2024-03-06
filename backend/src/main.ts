bcc3ecdc892a40bdbed47093b264931da9aae3cb
Joel Filho <joelfilho02812@gmail.com>
2024-03-06 17:09:38 -0300

    chore: add swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,12 +4,24 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Course Platform')
+    .setDescription('The Course Platform API')
+    .setVersion('1.0')
+    // .addTag('courses')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('course-platform/swagger-ui.html', app, document);
+
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
